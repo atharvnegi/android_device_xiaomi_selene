ab789838910fc44c8b6ad0059fa70dd836261278
Atharv Negi <94751339+AtarvNegi2951@users.noreply.github.com>
2022-01-26 18:40:19 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -508,3 +508,7 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0.vendor \
     android.hardware.usb@1.1 \
     android.hardware.usb@1.1.vendor
+    
+    # GCam
+    PRODUCT_PACKAGES += \
+    GCam
