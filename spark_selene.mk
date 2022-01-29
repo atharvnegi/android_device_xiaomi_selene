b0d074d489e82359cd0b6b6cd7f62a89bf069e0f
Atharv Negi <94751339+AtarvNegi2951@users.noreply.github.com>
2022-01-29 15:33:02 +0530

    Update and rename derp_selene.mk to spark_selene.mk

--- spark_selene.mk
+++ spark_selene.mk
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Bootanimation res
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -43,7 +43,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := selene
-PRODUCT_NAME := derp_selene
+PRODUCT_NAME := spark_selene
+PRODUCT_MODEL := Redmi 10 Prime
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := xiaomi
 
